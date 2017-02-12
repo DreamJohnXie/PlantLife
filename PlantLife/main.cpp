078e1b687a146d5f7c975a09378cebb659a8a5ed
DreamJohnXie <jiaxuan.xie@hotmail.com>
2017-02-11 23:15:51 -0500

    Initial Commit

---
+++ PlantLife/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlantLife
+//
+//  Created by Jiaxuan Xie on 2017-02-11.
+//  Copyright © 2017 Jiaxuan Xie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
